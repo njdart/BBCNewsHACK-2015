26e3a5bd5d3507f278cfd060055a6770b5f533c7
Owen Garland <garland.owen@gmail.com>
2015-03-19 13:25:48 +0000

    Node framework setup

--- server.js
+++ server.js
@@ -1,39 +1,60 @@
 // Load the http module to create an http server.
-var http = require('http'),
-    express = require('express'),
+var       http = require('http'),
+       express = require('express'),
     bodyParser = require('body-parser'),
-    app = express(),
-    server = require('http').createServer(app);
+        exphbs = require('express-handlebars'),
+           app = express(),
+        server = require('http').createServer(app);
 
 /* Express settings */
-app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+ /* Handlebar settings */
+app.engine('handlebars', exphbs({
+  defaultLayout: 'layout',
+  helpers: {
+    foo: function () { return 'FOO!'; }
+  },
+}));
+app.set('view engine', 'handlebars');
+
+/* Render home */
+app.get('/', function(req, res){
+  renderPage('home', null, res);
+});
+
+/* 404 Route */
+app.get('*', function(req, res){
+  res.status(404).send("404, these are not the droids you are looking for.");
+});
+
+/* Return a page with data included */
+function renderPage(page, data, res) {
+  res.render(page, {'data':data});
+}
 
 /* Start server */
 server.listen(8000);
 console.log("Server listening on localhost:8000");
 
-var url = "http://data.test.bbc.co.uk/bbcrd-juicer/articles/9de77ae4ae4f60738bcf18d004bf48a5711a05ab?apikey=YB0MY3VMHyllzPqEf5alVj5bUvGpvDVi";
+/* Ignore this for a while */
+function getArticle() {
+  var url = "http://data.test.bbc.co.uk/bbcrd-juicer/articles/9de77ae4ae4f60738bcf18d004bf48a5711a05ab?apikey=YB0MY3VMHyllzPqEf5alVj5bUvGpvDVi";
 
-http.get(url, function(res) {
-  var body = '';
+  http.get(url, function(res) {
+    var body = '';
 
-  res.on('data', function(chunk) {
-    body += chunk;
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      var response = JSON.parse(body);
+      console.log("Got response: %j", response);
+    });
+  }).on('error', function(e) {
+    console.log("Got error: ", e);
   });
-
-  res.on('end', function() {
-    var response = JSON.parse(body);
-    console.log("Got response: %j", response);
-  });
-}).on('error', function(e) {
-  console.log("Got error: ", e);
-});
-
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+}
