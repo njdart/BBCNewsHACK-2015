ba7a2bf3f457be65c5d2d0e762993b818805ed1c
LauraMPugh <lap24@aber.ac.uk>
2015-03-20 11:44:34 +0000

    Added new links

--- dev/testURL.js
+++ dev/testURL.js
@@ -7,13 +7,16 @@ var urls = [
   'http://www.independent.co.uk/news/world/asia/north-korea-ready-anytime-for-nuclear-war-as-diplomat-calls-defectors-animals-and-scum-10121587.html',
   'http://www.theguardian.com/education/2015/mar/20/labour-calls-time-on-exam-factory-approach-to-schooling',
   'http://www.ft.com/cms/s/eb98baa4-ce49-11e4-86fc-00144feab7de,Authorised=false.html?_i_location=http%3A%2F%2Fwww.ft.com%2Fcms%2Fs%2F0%2Feb98baa4-ce49-11e4-86fc-00144feab7de.html%3Fsiteedition%3Duk&siteedition=uk&_i_referer=http%3A%2F%2Fwww.ft.com%2Fhome%2Fuk#axzz3Uv0Y2KoV',
-  'http://www.telegraph.co.uk/finance/economics/11484544/Europe-squeezes-more-reforms-from-Greece-as-Merkel-steps-into-bail-out-talks.html',
+  http://www.telegraph.co.uk/finance/economics/11484544/Europe-squeezes-more-reforms-from-Greece-as-Merkel-steps-into-bail-out-talks.html',
   'http://www.southwales-eveningpost.co.uk/Adam-Jones-linked-English-Harlequins/story-26202114-detail/story.html',
   'http://www.walesonline.co.uk/news/wales-news/stag-party-thrown-ryanair-flight-8880743',
   'http://www.newstatesman.com/culture/2015/03/so-hot-right-now-peppers-prove-theres-perv-all-us',
-  'http://www.thetimes.co.uk/tto/life/motoring/article4387712.ece'
-];
-
+  'http://www.thetimes.co.uk/tto/life/motoring/article4387712.ece',
+  'http://www.bbc.co.uk/sport/0/football/31899097',
+  'http://www.bbc.co.uk/sport/football/teams/cardiff-city/results',
+  'http://www.telegraph.co.uk/news/picturegalleries/picturesoftheday/11451022/Pictures-of-the-day-5-March-2015.html?frame=3220760',
+  'http://www.bbc.co.uk/weather/2654092'
+  ];
 urls.map(function(url) { 
   getJuicerArticle(sha1URL(url), url);
 });
