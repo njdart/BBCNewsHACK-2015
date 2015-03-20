a08a77bbd024e3b5f30015f7fe4a2f0075d4cf47
Owen Garland <garland.owen@gmail.com>
2015-03-20 11:07:57 +0000

    hashing correct

--- server.js
+++ server.js
@@ -74,8 +74,8 @@ function getArticlesFromTweets(tweets) {
   var index = results.indexOf(url);
     if(results[url].length < 25) {
       var temps = results.splice(index, 1);
-      results[url] = sha1URL(results[url]);
     }
+    results[url] = sha1URL(results[url]);
   }
   createArticleList(results);
 }
