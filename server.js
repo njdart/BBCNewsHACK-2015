65454838c5b1a3a530579ab2363bb895de75b697
Nicholas Dart <nicholasjdart@gmail.com>
2015-03-20 15:34:52 +0000

    hard coded location for demo

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.route('/run').post(function(req, res) {
   //var twitterQuery = "Xander_barnes";
   // post.lat = 53.1436732;
   // post.lng = -4.2727924;
-  var locationData = post.lat + "," + post.lng + "," + post.radius + "mi";
+  var locationData = post.lat + "," + post.lng + "," + 20 + "mi";
   client.get('search/tweets', {q: twitterQuery,geocode:locationData, count:24, result_type: "recent"}, function(error, tweets, response) {
     getArticlesFromTweets(tweets); 
   });
